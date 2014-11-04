5c1ad33fa6c40cc38cdc09b61caee1918d4ae860
Lei Mingyu <uygnim.iel@gmail.com>
2014-11-05 02:35:35 +0800

    admin main page

--- routes.js
+++ routes.js
@@ -7,9 +7,12 @@
 var index = require('./controllers/index');
 var user  = require('./controllers/user');
 var movie = require('./controllers/movie');
+var admin = require('./controllers/admin');
 
 module.exports = function routes(app) {
 	app.get('/', index);
+
+	// user
 	app.get('/login', user.show_login);
 	app.get('/signup', user.show_signup);
 	app.get('/logout', user.logout);
@@ -18,6 +21,9 @@ module.exports = function routes(app) {
 	app.post('/user/signup', user.signup);
 	app.post('/user/login', user.login);
 	app.post('/user/profile', user.profile);
+
+	// admin
+	app.get('/admin', admin.show);
 
 	app.post('/movie/homepage_movie_search',movie.homepage_movie_search);
 	
