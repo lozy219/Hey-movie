9084bc5e5e4ee9685736fac2e974e6a85888df15
Lei Mingyu <uygnim.iel@gmail.com>
2014-11-12 23:01:32 +0800

    try to use bootstrap datetimepicker

--- controllers/user.js
+++ controllers/user.js
@@ -28,11 +28,11 @@ exports.show_signup = function* (){
 };
 
 exports.show_profile = function* (){
-	this.body = yield render('profile', {user : this.session.customer});
+	this.body = yield render('user/profile', {user : this.session.customer});
 };
 
 exports.show_profile_edit = function* (){
-	this.body = yield render('profile_edit', {user : this.session.customer});
+	this.body = yield render('user/profile_edit', {user : this.session.customer});
 };
 
 exports.signup = function* (){
