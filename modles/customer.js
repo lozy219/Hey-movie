29c31b8eb202b6dc105cfb482b0ffded9d0dd063
fanshicomic <fanshicomic@gmail.com>
2014-11-13 10:34:37 +0800

    fix bug in signup

--- modles/customer.js
+++ modles/customer.js
@@ -5,8 +5,7 @@ var db = require('./db.js');
 exports.insert = function* (customer) {
 	var name = yield db.get_customer_by_username(customer.name);
 	var email = yield db.get_customer_by_email(customer.email);
-
-	if (name.length == 0 && name.length == 0){
+	if (name.length == 0 && email.length == 0){
 		return yield db.add_customer(customer);
 	} else {
 		return false;
